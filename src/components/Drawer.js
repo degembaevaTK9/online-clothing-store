5cfe02c98447366a9ebed35b22a5a9b3067d5163
Taazim <tdkoroleva98@gmail.com>
2022-05-26 01:09:12 +0600

    worked on the layout of the main page and the shopping cart page, divided into components

---
+++ src/components/Drawer.js
@@ -0,0 +1,62 @@
+function Drawer() {
+  return (
+    <div style={{ display: "none" }} className="overlay">
+      <div className="drawer">
+        <h2 className="d-flex justify-between mb-30">
+          Корзина{" "}
+          <img className="cu-p" src="/img/btn-remove.svg" alt="Remove" />
+        </h2>
+        <div className="items">
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              className="cartItemImg"
+              style={{
+                backgroundImage: 'url("/img/sneakers/sneakers 1.jpg")',
+              }}
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 сом</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+
+          <div className="cartItem d-flex align-center mb-20">
+            <div
+              className="cartItemImg"
+              style={{
+                backgroundImage: 'url("/img/sneakers/sneakers 1.jpg")',
+              }}
+            ></div>
+            <div className="mr-20 flex">
+              <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+              <b>12 999 сом</b>
+            </div>
+            <img className="removeBtn" src="/img/btn-remove.svg" alt="Remove" />
+          </div>
+        </div>
+
+        <div className="cartTotalBlock">
+          <ul>
+            <li>
+              <span>Итого:</span>
+              <div></div>
+              <b>5000 сом</b>
+            </li>
+            <li>
+              <span>Налог 5%:</span>
+              <div></div>
+              <b>100 сом</b>
+            </li>
+          </ul>
+
+          <button className="greenButton">
+            Оформить заказ <img src="/img/arrow.svg" alt="Arrow" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Drawer;
